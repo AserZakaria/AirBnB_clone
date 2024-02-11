a440d148071addbea251bf5bb221f5db918152b2
AserZakaria <asserzakaria@gmail.com>
2024-02-11 10:04:13 -0800

    A

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""Defines unittests for models/engine/file_storage.py.
-
-Unittest classes:
-    TestFileStorage_instantiation
-    TestFileStorage_methods
-"""
+"""Defines unittests for models/engine/file_storage.py."""
 import os
 import json
 import models
